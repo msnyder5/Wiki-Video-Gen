1069731e3262bcb0ef0ab1e91ce35f56a924d339
Matthew <msnyder2@terpmail.umd.edu>
2024-03-27 00:32:04 -0400

    it makes a video, but the graphics suck. see ya in a few days

--- main.py
+++ main.py
@@ -1,27 +1,14 @@
-import random
+from dotenv import load_dotenv
+
+load_dotenv()
 
 from wiki2vid import Wiki2Vid
 
 
-def random_wiki_url():
-    return random.choice(
-        [
-            "https://apexlegends.fandom.com/wiki/Fuse",
-            "https://apexlegends.fandom.com/wiki/Bloodhound",
-        ]
-    )
-
-
 def main():
-    wiki_url = "https://en.wikipedia.org/wiki/Python_(programming_language)"
-    wiki2vid = Wiki2Vid(wiki_url)
-    wiki2vid.run()
-
-
-def test():
     wiki2vid = Wiki2Vid("https://en.wikipedia.org/wiki/Cottage_cheese_boycott")
     wiki2vid.run()
 
 
 if __name__ == "__main__":
-    test()
+    main()
--- wiki2vid/audio.py
+++ wiki2vid/audio.py
@@ -1,9 +1,14 @@
+import os
+
+from elevenlabs.client import ElevenLabs
+
 from wiki2vid.segment import Content
 
 
 class AudioBuilder:
     def __init__(self, content: Content):
         self.content = content
+        self.client = ElevenLabs(api_key=os.getenv("ELEVENLABS_API_KEY"))
 
     def build_audio(self) -> None:
         pass
